a42028235a00c0a26392de31e40a0e952e02b4a7
Sven Reiser <svreiser@gmail.com>
2023-05-28 16:06:03 +0200

    replaces graphics and adds base setup

--- src/game.ts
+++ src/game.ts
@@ -1,130 +1,141 @@
-import * as Phaser from 'phaser';
-class Example extends Phaser.Scene
-{
-    scoreText;
-    score = 0;
-    cursors;
-    platforms;
-    stars;
-    player;
+import * as Phaser from "phaser";
+class Example extends Phaser.Scene {
+  scoreText;
+  score = 0;
+  cursors;
+  platforms;
+  stars;
+  player;
 
-    preload ()
-    {
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 'assets/dick.png', { frameWidth: 96, frameHeight: 96 });
+  preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/ground.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dick.png", {
+      frameWidth: 96,
+      frameHeight: 96,
+    });
+  }
+
+  create() {
+    this.add.image(500, 300, "sky");
+
+    this.platforms = this.physics.add.staticGroup();
+
+    this.platforms.create(50, 575, "ground");
+    this.platforms.create(150, 575, "ground");
+    this.platforms.create(250, 575, "ground");
+    this.platforms.create(350, 575, "ground");
+    this.platforms.create(450, 575, "ground");
+    this.platforms.create(450, 525, "ground");
+    this.platforms.create(550, 575, "ground");
+    this.platforms.create(650, 575, "ground");
+    this.platforms.create(750, 575, "ground");
+    this.platforms.create(850, 575, "ground");
+    this.platforms.create(950, 575, "ground");
+
+    this.player = this.physics.add.sprite(100, 450, "dude");
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 2 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.stars = this.physics.add.group({
+      key: "star",
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 },
+    });
+
+    this.stars.children.iterate((child) => {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.scoreText = this.add.text(16, 16, "score: 0", { fontSize: "32px" });
+
+    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.stars, this.platforms);
+
+    this.physics.add.overlap(
+      this.player,
+      this.stars,
+      this.collectStar,
+      null,
+      this
+    );
+  }
+
+  update() {
+    var pointer = this.input.activePointer;
+    // Auto Move Player
+    this.player.setVelocityX(160);
+    this.player.anims.play("left", true);
+    if (
+      this.player.body.touching.down &&
+      (pointer.isDown || this.cursors.up.isDown)
+    ) {
+      this.player.setVelocityY(-800);
+      console.log("test");
     }
+  }
 
-    create ()
-    {
-        this.add.image(400, 300, 'sky');
+  collectStar(player, star) {
+    star.disableBody(true, true);
 
-        this.platforms = this.physics.add.staticGroup();
-
-        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-        this.platforms.create(600, 400, 'ground');
-        this.platforms.create(50, 250, 'ground');
-        this.platforms.create(750, 220, 'ground');
-
-        this.player = this.physics.add.sprite(100, 450, 'dude');
-
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [ { key: 'dude', frame: 2 } ],
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.cursors = this.input.keyboard.createCursorKeys();
-
-        this.stars = this.physics.add.group({
-            key: 'star',
-            repeat: 11,
-            setXY: { x: 12, y: 0, stepX: 70 }
-        });
-
-        this.stars.children.iterate(child =>
-        {
-
-            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-        });
-
-        this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px' });
-
-        this.physics.add.collider(this.player, this.platforms);
-        this.physics.add.collider(this.stars, this.platforms);
-
-        this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
-    }
-
-    update ()
-    {
-        if (this.cursors.left.isDown)
-        {
-            this.player.setVelocityX(-160);
-
-            this.player.anims.play('left', true);
-        }
-        else if (this.cursors.right.isDown)
-        {
-            this.player.setVelocityX(160);
-
-            this.player.anims.play('right', true);
-        }
-        else
-        {
-            this.player.setVelocityX(0);
-
-            this.player.anims.play('turn');
-        }
-
-        if (this.cursors.up.isDown && this.player.body.touching.down)
-        {
-            this.player.setVelocityY(-330);
-        }
-    }
-
-    collectStar (player, star)
-    {
-        star.disableBody(true, true);
-
-        this.score += 10;
-        this.scoreText.setText(`Score: ${this.score}`);
-    }
+    this.score += 10;
+    this.scoreText.setText(`Score: ${this.score}`);
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    width: 800,
+  type: Phaser.AUTO,
+  width: 1000,
+  height: 600,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    parent: "phaser-example",
+    width: 1000,
     height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
+    min: {
+      width: 1000,
+      height: 600,
     },
-    scene: Example
+    max: {
+      width: 1600,
+      height: 1200,
+    },
+  },
+
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 1500 },
+      debug: false,
+    },
+  },
+  scene: Example,
 };
 
 const game = new Phaser.Game(config);
